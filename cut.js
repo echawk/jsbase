d85f7a443cfa6fdf24a78ce3318b00ce5b7edc3b
Ethan <ethan.hawk@valpo.edu>
2021-07-27 11:43:41 -0500

    cut.js: started work on parsing field values, utilize util.js, refactoring

--- cut.js
+++ cut.js
@@ -7,13 +7,36 @@ cut's flags all start with a dash, then are followed by a letter
 with the option to each flag, being the next value
 */
 var args = scriptArgs.slice(1)
-var i = 0
 var delimiter
 var field // will need to make an array to follow spec
 var files = []
+let fields = [] /* I think it will be easier to list the fields to *drop* rather than to list the fields to keep*/
+
+function parseFieldArg (arg) {
+	let comma_split_arg = arg.split(",")
+	for (let i = 0; i < comma_split_arg.length; i++) {
+		if (comma_split_arg[i].includes("-")) {
+			if (comma_split_arg[i].startsWith("-")) {
+				for (let i = 1; i <= Number.parseInt(dash_split_arg[1]); i++) {
+					fields.push(i)
+				}
+			} else if (comma_split_arg[i].startsWith("-")) {
+					/* FIXME: follow spec */
+			} else {
+				let dash_split_arg = comma_split_arg[i].split("-")
+				for (let i = Number.parseInt(dash_split_arg[0]); i <= Number.parseInt(dash_split_arg[1]); i++) {
+					print(i)
+					fields.push(i)
+				}
+			}
+		} else {
+			fields.push(Number.parseInt(comma_split_arg[i]))
+		}
+	}
+}
 
 /* Parse arguments */
-while (i < args.length) {
+for (let i = 0; i < args.length; i++) {
 	if (args[i].startsWith("-d")) {
 		if (!(args[i].replace("-d", "") == "")) {
 			delimiter = args[i].replace("-d", "")
@@ -27,18 +50,21 @@ while (i < args.length) {
 		if (!(args[i].replace("-f", "") == "")) {
 			/* FIXME: follow spec */
 			field = Number.parseInt(args[i].replace("-f", "")) - 1
+			parseFieldArg(args[i].replace("-f", ""))
 		} else {
 			i++
 			if (i < args.length) {
+				parseFieldArg(args[i])
 				if(isFinite(args[i])) {
 					field = Number.parseInt(args[i]) - 1
 				}
 			}
 		}
+	} else if (args[i] == "-") {
+		/* FIXME: follow spec */
 	} else {
 		files.push(args[i])
 	}
-	i++
 }
 /* by default, the delmiting character is a tab */
 if (delimiter == "") {
@@ -58,31 +84,16 @@ var outbuffer = []
 /* if no files, act on standard input */
 if (files.length == 0) {
 	var lines = util.readStdinAsLines()
-	i = 0
-	while (i < lines.length) {
+	for (let i = 0; i < lines.length; i++) {
 		outbuffer.push(lines[i].split(delimiter)[field])
-		i++
 	}
 } else {
-	i = 0
-	while (i < files.length) {
-		var file = std.open(files[i], "r")
-		if (file == null) {
-			std.exit(1)
+	for (let i = 0; i < files.length; i++) {
+		let lines = util.readFileAsLines(files[i])
+		for (let j = 0; j < lines.length; j++) {
+			outbuffer.push(lines[j].split(delimiter)[field])
 		}
-		while (!file.eof()) {
-			var l = file.getline()
-			if (l != null) {
-				outbuffer.push(l.split(delimiter)[field])
-			}
-		}
-		file.close()
-		i++
 	}
 }
 
-i = 0
-while (i < outbuffer.length) {
-	print(outbuffer[i])
-	i++
-}
+util.printLines(outbuffer)
