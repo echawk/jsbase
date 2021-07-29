d9cf37a43b9fa09b433a03981de953ca1225aeca
Ethan <ethan.hawk@valpo.edu>
2021-07-28 21:53:19 -0500

    basename.js: implement basename

--- basename.js
+++ basename.js
@@ -1,12 +1,27 @@
 import * as os from "os";
 import * as std from "std";
-/* cut -d',' -f2 -- DELIM=, FIELD=2 */
-/*
-cut's flags all start with a dash, then are followed by a letter
-with the option to each flag, being the next value
 
-*/
-
-console.log(scriptArgs)
-
-console.log(JSON.stringify(scriptArgs))
+let args = scriptArgs.slice(1)
+if (args.length <= 2) {
+	let string = args[0]
+	if (string == null) {
+		std.exit(1)
+	} else {
+		if (string == "//") {
+			std.printf("/\n")
+			std.exit(0)
+		} else if (string.replaceAll("/", "") == "") {
+			std.printf("/\n")
+			std.exit(0)
+		}
+		string = string.endsWith("/") ? string.substring(0, string.length - 1) : string
+		string = string.includes("/") ? string.slice(string.lastIndexOf("/") + 1) : string
+		if (args.length == 2) {
+			let suffix = args[1]
+			string = string.endsWith(suffix) ? string.substring(0, string.lastIndexOf(suffix)) : string
+		}
+		std.printf("%s\n", string)
+	}
+} else {
+	std.exit(1)
+}
